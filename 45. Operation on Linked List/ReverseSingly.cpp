62df1e3c53ab1e8a872eee336bf0bcb727bf0945
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-21 17:40:59 +0530

    Reversal of Singly Linked List

---
+++ 45. Operation on Linked List/ReverseSingly.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+	public:
+	int data;
+	Node* next;
+
+	Node(int data){
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+void InsertAtHead(Node* &h, Node* &t, int data){
+	if(!h){
+		h = new Node(data);
+		t = h;
+		return;
+	}
+	Node* temp = new Node(data);
+	temp->next = h;
+	h = temp;
+}
+
+void InsertAtTail(Node* &h, Node* &t, int data){
+	if(!h){
+		h = new Node(data);
+		t = h;
+		return;
+	}
+	Node* temp = new Node(data);
+	t->next = temp;
+	t = temp;
+}
+
+void InsertAtPos(Node* &h, Node* &t, int data, int pos){
+	if(pos<1){
+		cout << "Invalid position. Cannot insert" << endl;
+		return;
+	}
+	if(pos == 1){
+		InsertAtHead(h, t, data);
+		return;
+	}
+	Node* temp = h;
+	int count = 1;
+	while(temp && count < pos-1){
+		temp = temp->next;
+		count++;
+	}
+
+	if(!temp){
+		cout << "Current position is out of bounds" << endl;
+		return;
+	}
+
+	Node* newNode = new Node(data);
+	newNode->next = temp->next;
+	temp->next = newNode;
+}
+
+void DeleteAtHead(Node* &h){
+	if(h == NULL){
+		cout << "List is empty. Nothing to delete" << endl;
+		return;
+	}
+	Node* temp = h;
+	h = h->next;
+	delete temp;
+}
+
+void DeleteAtTail(Node* &h, Node* &t){
+	if(h == NULL){
+		cout << "List is empty. Nothing to delete" << endl;
+		return;
+	}
+	Node* temp = h;
+	while(temp->next != t)
+		temp = temp->next;
+
+	temp->next = NULL;
+	delete t;
+	t = temp;
+	t->next = NULL;
+}
+
+void DeleteAtPos(Node* &h, int pos){
+	if(pos<1){
+		cout << "Invalid position, unable to delete" << endl;
+		return;
+	}
+	if(pos == 1){
+		DeleteAtHead(h);
+		return;
+	}
+	Node* temp = h;
+	int count = 1;
+	while(temp && count < pos-1){
+		temp = temp->next;
+		count++;
+	}
+	
+	if(!temp){
+		cout << "Position is out of bounds" << endl;
+		return;
+	}
+	Node* deleteNode = temp->next;
+	if(deleteNode == NULL){
+		cout << "Position is out of bounds" << endl;
+		delete deleteNode;
+		return;
+	}
+
+	temp->next = deleteNode->next;
+	delete deleteNode;
+}
+
+void print(Node* h){
+	if(h == NULL){
+		cout << "List is empty. Nothing to delete" << endl;
+		return;
+	}
+	Node* temp = h;
+	cout << "Linked List: ";
+	while(temp != NULL){
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+void reverse(Node* &h){
+	Node* prev = NULL;
+	Node* curr = h;
+	Node* forward;
+	while(curr != NULL){
+		forward = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = forward;
+	}
+	h = prev;
+}
+
+int main(){
+	Node* h = NULL;
+	Node* t = NULL;
+	InsertAtHead(h, t, 30);
+	InsertAtHead(h, t, 20);
+	InsertAtHead(h, t, 10);
+	InsertAtTail(h, t, 40);
+	InsertAtPos(h, t, 50, 3);
+	print(h);
+	// DeleteAtHead(h);
+	// DeleteAtTail(h, t);
+	// DeleteAtPos(h, 10);
+	// print(h);
+	reverse(h);
+	cout << "Linked List After Reversal: ";
+	print(h);
+	return 0;
+}
