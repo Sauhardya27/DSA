a04a2923ceb5c053927fde3c40c2376cfc63f71b
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-03-27 20:07:03 +0530

    Cycle detection in directed graph

---
+++ 89. Cycle detection in directed graph/CycleDetectionDirected.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+
+bool isCycleDFS(int node, unordered_map<int, bool> &visited, unordered_map<int, bool> &dfsVisited, unordered_map<int, list<int>> &adj){
+  visited[node] = true;
+  dfsVisited[node] = true;
+
+  for(auto neighbour: adj[node]){
+    if(!visited[neighbour]){
+      bool cycleDetected = isCycleDFS(neighbour, visited, dfsVisited, adj);
+      if(cycleDetected)
+        return true;
+    }
+    else if(dfsVisited[neighbour])
+      return true;
+  }
+
+  dfsVisited[node] = false;
+  return false;
+}
+
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  unordered_map<int, list<int>> adj;
+
+  //Create adjacency list
+  for(int i=0; i<edges.size(); i++){
+    int u = edges[i].first;
+    int v = edges[i].second;
+
+    adj[u].push_back(v);
+  }
+  unordered_map<int, bool> visited;
+  unordered_map<int, bool> dfsVisited;
+
+  //Check for all the vertices and disconnected components
+  for(int i=1; i<=n; i++){
+    if(!visited[i]){
+      bool ans = isCycleDFS(i, visited, dfsVisited, adj);
+      if(ans)
+        return 1;
+    }
+  }
+  return 0;
+}
+
+int main()
+{
+	int tc;
+	cin >> tc;
+	while (tc--)
+	{
+		int n, m;
+		cout << "Enter no. of vertices and edges: ";
+		cin >> n >> m;
+
+		vector<pair < int, int > > edges(m, {0, 0});
+		for (int i = 0; i < m; i++)
+		{
+			int u, v;
+        	cin >> u >> v;
+        	edges.push_back({u,v});
+		}
+
+		if(detectCycleInDirectedGraph(n, edges))
+			cout << "true" << endl;
+		else
+			cout << "false" << endl;
+	}
+
+	return 0;
+}
