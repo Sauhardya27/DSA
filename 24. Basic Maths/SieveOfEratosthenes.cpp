0482580076d3053bc3ceb9dd07b3f0ca6b244285
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-07-18 12:00:52 +0530

    Sieve of eratosthenes

---
+++ 24. Basic Maths/SieveOfEratosthenes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPrimes(int n){
+	// Sieve of Eratosthenes
+	int count = 0;
+	vector<bool> prime(n + 1, true);
+
+	prime[0] = prime[1] = false;
+
+	for (int i = 2; i < n; i++){
+		if (prime[i]){
+			count++;
+
+			for (int j = 2 * i; j < n; j += i)
+				prime[j] = false;
+		}
+	}
+	return count;
+}
+
+int main(){
+	cout << "No. of prime numbers between 0-100: " << countPrimes(100) << endl;
+	return 0;
+}
