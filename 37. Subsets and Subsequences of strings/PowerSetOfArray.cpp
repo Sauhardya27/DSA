a568d89eb1b9ef3643b7190fd11eb6527185809f
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-07-19 23:44:28 +0530

    Power set of an array

---
+++ 37. Subsets and Subsequences of strings/PowerSetOfArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>> &ans){
+	if (index >= nums.size()){
+		ans.push_back(output);
+		return;
+	}
+
+	// exclude
+	solve(nums, output, index + 1, ans);
+
+	// include
+	int element = nums[index];
+	output.push_back(element);
+	solve(nums, output, index + 1, ans);
+}
+
+vector<vector<int>> subsets(vector<int> &nums){
+	vector<vector<int>> ans;
+	vector<int> output;
+	int index = 0;
+
+	solve(nums, output, index, ans);
+
+	return ans;
+}
+
+int main(){
+	vector<int> arr = {1, 2, 3};
+	vector<vector<int>> ans = subsets(arr);
+
+	cout << "Subsets:" << endl;
+    for (const auto &subset : ans) {
+        cout << "{ ";
+        for (int num : subset) {
+            cout << num << " ";
+        }
+        cout << "}" << endl;
+    }
+	return 0;
+}
