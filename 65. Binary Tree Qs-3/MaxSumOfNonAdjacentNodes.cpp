60ce157447eaec0c0c6200eb90961947f14ed3c1
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-08-29 23:25:11 +0530

    FInd the maximum sum of non-adjacent nodes

---
+++ 65. Binary Tree Qs-3/MaxSumOfNonAdjacentNodes.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node *left;
+	Node *right;
+};
+
+pair<int, int> solve(Node *root)
+{
+	if (root == NULL)
+	{
+		pair<int, int> p = make_pair(0, 0);
+		return p;
+	}
+
+	pair<int, int> leftAns = solve(root->left);
+	pair<int, int> rightAns = solve(root->right);
+
+	pair<int, int> res;
+
+	res.first = root->data + leftAns.second + rightAns.second;
+	res.second = max(leftAns.first, leftAns.second) + max(rightAns.first, rightAns.second);
+
+	return res;
+}
+
+int getMaxSum(Node *root)
+{
+	pair<int, int> ans = solve(root);
+	return max(ans.first, ans.second);
+}
