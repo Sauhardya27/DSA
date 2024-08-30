619f01e664c379c9e9b1883f3012ff65b25db9ae
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-08-30 21:24:38 +0530

    Left and Right view of a binary tree

---
+++ 64. Binary Tree Qs-2/LeftViewOfBinaryTree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(int x)
+	{
+		data = x;
+		left = right = NULL;
+	}
+};
+
+void solve(Node *root, vector<int> &ans, int lvl)
+{
+	if (root == NULL)
+		return;
+
+	if (lvl == ans.size())
+		ans.push_back(root->data);
+
+	solve(root->left, ans, lvl + 1);
+	solve(root->right, ans, lvl + 1);
+}
+
+vector<int> leftView(Node *root)
+{
+	vector<int> ans;
+	int lvl = 0;
+	solve(root, ans, lvl);
+	return ans;
+}
