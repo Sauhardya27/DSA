84411408deb8b2a55307c5c192665b12658f172e
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-10-18 11:58:57 +0530

    Implement Median in a stream class

---
+++ 77. Heap Qs-3/implementMedianInAStream.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    priority_queue<int> maxHeap;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    
+    public:
+    void insertHeap(int &x)
+    {
+        if (maxHeap.empty() || x <= maxHeap.top()) {
+            maxHeap.push(x);
+        } else {
+            minHeap.push(x);
+        }
+        balanceHeaps();
+    }
+    
+    void balanceHeaps()
+    {
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double getMedian()
+    {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return double(maxHeap.top());
+        }
+    }
+};
+
+int main()
+{
+    int n, x;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    	Solution ob;
+    	cin >> n;
+    	for(int i = 1;i<= n; ++i)
+    	{
+    		cin >> x;
+    		ob.insertHeap(x);
+    	    cout << floor(ob.getMedian()) << endl;
+    	}
+    }
+	return 0;
+}
