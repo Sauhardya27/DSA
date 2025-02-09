f7dac004e5a5bff54a2cb0135b2099cbe3ddedbe
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2025-02-09 11:29:36 +0530

    Longest Increasing Subsequence in O(Nlog N) T.C. and O(N) S.C.

---
+++ 119. Longest Increasing Subsequnece/LISOptimal.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solveOptimal(int n, vector<int> &arr){
+	if(n == 0)
+		return 0;
+		
+	vector<int> ans;
+	ans.push_back(arr[0]);
+	
+	for(int i=1; i<n; i++){
+		if(arr[i] > ans.back())
+			ans.push_back(arr[i]);
+		else {
+			int index = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin();
+			//Using binary search to find out the index to replace
+			ans[index] = arr[i];
+		}
+	}
+	return ans.size();
+}
+
+int lis(vector<int>& arr) {
+	int n = arr.size();
+	return solveOptimal(n, arr);
+}
